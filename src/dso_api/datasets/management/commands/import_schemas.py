0df55d66369ed9620318a11fadd3512c0b204a4d
Diederik van der Boor <vdboor@edoburu.nl>
2020-01-28 13:12:20 +0100

    Improve robustness of import_schemas for unresolved references

--- src/dso_api/datasets/management/commands/import_schemas.py
+++ src/dso_api/datasets/management/commands/import_schemas.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
 from django.db import transaction, DatabaseError
 from schematools.db import create_tables
 from schematools.schema.utils import schema_defs_from_url
@@ -9,8 +9,12 @@ from dso_api.datasets.models import Dataset
 
 class Command(BaseCommand):
     help = "Import all known Amsterdam schema files."
+    requires_system_checks = False
 
     def handle(self, *args, **options):
+        errors = 0
+        self.stdout.write(f"Loading schema from {settings.SCHEMA_URL}")
+        datasets = []
         for name, schema in schema_defs_from_url(settings.SCHEMA_URL).items():
 
             if Dataset.objects.filter(name=schema.id).exists():
@@ -18,13 +22,25 @@ class Command(BaseCommand):
                 continue
 
             self.stdout.write(f"* Importing {name}")
-            Dataset.objects.create(
+            dataset = Dataset.objects.create(
                 name=schema.id,
                 schema_data=schema.json_data()
             )
+            datasets.append(dataset)
 
-            try:
-                with transaction.atomic():
-                    create_tables(schema)
-            except DatabaseError as e:
-                self.stderr.write(f"  Tables not created: {e}")
+        # Create the tables for all these datasets
+        if not datasets:
+            self.stdout.write(f"No new datasets imported")
+        else:
+            self.stdout.write(f"Creating tables")
+            for dataset in datasets:
+                try:
+                    self.stdout.write(f"* Creating {dataset.name}")
+                    with transaction.atomic():
+                        create_tables(dataset.schema)
+                except (DatabaseError, ValueError) as e:
+                    self.stderr.write(f"  Tables not created: {e}")
+                    errors += 1
+
+        if errors:
+            raise CommandError("Not all datasets imported successfully")
