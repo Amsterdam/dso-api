1be77363bc1dce389323a2ff75061113a3ce506b
Diederik van der Boor <vdboor@edoburu.nl>
2020-01-28 16:32:29 +0100

    Avoid database queries for collectstatic/compilemessages
    This fixes the docker build

--- src/dso_api/dynamic_api/apps.py
+++ src/dso_api/dynamic_api/apps.py
@@ -11,8 +11,10 @@ class DynamicAPIApp(AppConfig):
         from dso_api.datasets.models import Dataset
         from dso_api.dynamic_api.urls import router
 
-        # Don't query the non-test database for the datasets
-        if "pytest" in sys.modules:
+        # Don't query the non-test database for the datasets,
+        # nor perform queries for management cool
+        command = sys.argv[1] if len(sys.argv) >= 2 else ''
+        if "pytest" in sys.modules or command in ('collectstatic', 'compilemessages'):
             return
 
         # Only start the reload when the project already migrated the base tables:
