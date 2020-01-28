f00830ddee7767b8a0c932eb812409e1d69acbbe
Jan Murre <jan.murre@gmail.com>
2020-01-28 15:59:21 +0100

    Create Dataset on POST.

--- src/dso_api/datasets/views.py
+++ src/dso_api/datasets/views.py
@@ -1,8 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from schematools.schema.types import DatasetSchema
+from schematools.db import create_tables
+
+from dso_api.datasets.models import Dataset
+
 
 class SchemaUploadView(APIView):
     def post(self, request):
-        breakpoint()
-        return Response("ok")
+        schema = DatasetSchema.from_dict(request.data)
+        Dataset.objects.create(name=schema.id, schema_data=schema.json_data())
+        create_tables(schema)
+        return Response(f"Dataset {schema.id} created")
