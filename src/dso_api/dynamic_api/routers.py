3450cdc239ae03eba3182a016f548162f919317b
Diederik van der Boor <vdboor@edoburu.nl>
2020-01-30 18:10:57 +0100

    Fixed generated detail links for related models

--- src/dso_api/dynamic_api/routers.py
+++ src/dso_api/dynamic_api/routers.py
@@ -10,7 +10,7 @@ from rest_framework import routers
 
 from dso_api.datasets.models import Dataset
 from dso_api.dynamic_api.locking import lock_for_writing
-from dso_api.dynamic_api.serializers import serializer_factory
+from dso_api.dynamic_api.serializers import serializer_factory, get_view_name
 from dso_api.dynamic_api.views import viewset_factory
 
 logger = logging.getLogger(__name__)
@@ -96,9 +96,7 @@ class DynamicRouter(routers.SimpleRouter):
         # Return which models + urls were generated
         result = {}
         for model in models:
-            viewname = (
-                f"dynamic_api:{model.get_dataset_id()}-{model.get_table_id()}-list"
-            )
+            viewname = get_view_name(model, "list")
             try:
                 url = reverse(viewname)
             except NoReverseMatch as e:
--- src/dso_api/dynamic_api/serializers.py
+++ src/dso_api/dynamic_api/serializers.py
@@ -13,6 +13,33 @@ class DynamicSerializer(DSOSerializer):
 
     table_schema: DatasetTableSchema = None
 
+    def build_url_field(self, field_name, model_class):
+        """Make sure the generated URLs point to our dynamic models"""
+        field_class = self.serializer_url_field
+        field_kwargs = {
+            "view_name": get_view_name(model_class, "detail"),
+        }
+
+        return field_class, field_kwargs
+
+    def build_relational_field(self, field_name, relation_info):
+        field_class, field_kwargs = super().build_relational_field(
+            field_name, relation_info
+        )
+        if "view_name" in field_kwargs:
+            model_class = relation_info[1]
+            field_kwargs["view_name"] = get_view_name(model_class, "detail")
+
+        return field_class, field_kwargs
+
+
+def get_view_name(model: Type[DynamicModel], suffix: str):
+    """Return the URL pattern for a dynamically generated model.
+
+    :param suffix: This can be "detail" or "list".
+    """
+    return f"dynamic_api:{model.get_dataset_id()}-{model.get_table_id()}-{suffix}"
+
 
 @lru_cache()
 def serializer_factory(model: Type[DynamicModel]) -> Type[DynamicSerializer]:
