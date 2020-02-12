7252bb12bec8538524111bc0a37527988626f446
Bart Heupers <bart.heupers@gmail.com>
2020-02-12 09:18:27 +0100

    WIP

--- src/dso_api/dbrouters.py
+++ src/dso_api/dbrouters.py
@@ -1,13 +1,13 @@
 class BagV11Router:
     """
     A router to control all database operations on models in the
-    auth and contenttypes applications.
+    apps that have data in other databases like bag
     """
-    route_app_labels = {'bag_grootstedelijkgebied'}
+    route_app_labels = {'bag'}
 
     def db_for_read(self, model, **hints):
         """
-        Attempts to read auth and contenttypes models go to auth_db.
+        Attempts to read bag go to bag_v11.
         """
         if model._meta.app_label in self.route_app_labels:
             return 'bag_v11'
@@ -15,20 +15,20 @@ class BagV11Router:
 
     def db_for_write(self, model, **hints):
         """
-        Attempts to write auth and contenttypes models go to auth_db.
+        Attempts to write.
         """
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         """
-        Allow relations if a model in the auth or contenttypes apps is
+        Allow relations if a model in the bag apps is
         involved.
         """
-        # if (
-        #     obj1._meta.app_label in self.route_app_labels or
-        #     obj2._meta.app_label in self.route_app_labels
-        # ):
-        #    return True
+        if (
+            obj1._meta.app_label in self.route_app_labels or
+            obj2._meta.app_label in self.route_app_labels
+        ):
+            return True
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
