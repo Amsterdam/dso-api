a4774970d64664416ef161e1b6d53ac7e513e515
jjmurre <jan.murre@gmail.com>
2020-02-26 12:02:45 +0100

    Added datapunt-authorization-django.

--- src/dso_api/settings.py
+++ src/dso_api/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "authorization_django.authorization_middleware",
 ]
 
 if DEBUG:
@@ -183,6 +184,34 @@ SWAGGER_SETTINGS = {
             "flow": "implicit",
         }
     },
+}
+
+# -- Amsterdam oauth settings
+
+# The following JWKS data was obtained in the authz project :  jwkgen -create -alg ES256
+# This is a test public/private key def and added for testing .
+JWKS_TEST_KEY = """
+    {
+        "keys": [
+            {
+                "kty": "EC",
+                "key_ops": [
+                    "verify",
+                    "sign"
+                ],
+                "kid": "2aedafba-8170-4064-b704-ce92b7c89cc6",
+                "crv": "P-256",
+                "x": "6r8PYwqfZbq_QzoMA4tzJJsYUIIXdeyPA27qTgEJCDw=",
+                "y": "Cf2clfAfFuuCB06NMfIat9ultkMyrMQO9Hd2H7O9ZVE=",
+                "d": "N1vu0UQUp0vLfaNeM0EDbl4quvvL6m_ltjoAXXzkI3U="
+            }
+        ]
+    }
+"""
+
+DATAPUNT_AUTHZ = {
+    "JWKS": os.getenv("PUB_JWKS", JWKS_TEST_KEY),
+    "ALWAYS_OK": False,
 }
 
 # -- Local app settings
