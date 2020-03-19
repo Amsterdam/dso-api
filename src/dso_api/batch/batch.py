daefdde4ab6bacbaadc9f72d72a8b4f173752732
Bart Heupers <bart.heupers@gmail.com>
2020-03-19 21:27:36 +0100

    First model dynamic model import

---
+++ src/dso_api/batch/batch.py
@@ -0,0 +1,64 @@
+import gc
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class BasicJob:
+    """Interface for jobs"""
+
+    name = None
+
+    def tasks(self) -> list:
+        pass
+
+
+def execute(job: BasicJob):
+    log.info("Starting job: %s [%s]", job.name, job.__class__.__name__)
+
+    for task in job.tasks():
+        _execute_task(task)
+
+    log.info("Finished job: %s: [%s]", job.name, job.__class__.__name__)
+
+
+def _execute_task(task):
+
+    if callable(task):
+        task_name = task.__name__
+        execute_func = task
+    else:
+        task_name = getattr(task, "name", "no name specified")
+        execute_func = task.execute
+
+    log.debug("Starting task: %s", task_name)
+
+    execute_func()
+
+
+class BasicTask:
+    """
+    Abstract task that splits execution into three parts:
+
+    * ``before``
+    * ``process``
+    * ``after``
+
+    """
+
+    name = "Basic Task"
+
+    def execute(self):
+        self.before()
+        self.process()
+        self.after()
+        gc.collect()
+
+    def before(self):
+        pass
+
+    def after(self):
+        pass
+
+    def process(self):
+        pass
