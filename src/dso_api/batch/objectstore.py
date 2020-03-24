06819a5406306a99da0756acd4e9c91ac1f507f7
Bart Heupers <bart.heupers@gmail.com>
2020-03-24 13:05:25 +0100

    First working CSV import

---
+++ src/dso_api/batch/objectstore.py
@@ -0,0 +1,65 @@
+import logging
+import os
+from functools import lru_cache
+from pathlib import Path
+
+from swiftclient.client import Connection
+
+from dso_api import settings
+
+log = logging.getLogger(__name__)
+
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("swiftclient").setLevel(logging.WARNING)
+
+container = os.getenv("GOB_OBJECTSTORE_ENV", "productie")
+
+connection = {
+    "auth_version": "2.0",
+    "authurl": "https://identity.stack.cloudvps.com/v2.0",
+    "user": "GOB_user",
+    "key": os.getenv("GOB_OBJECTSTORE_PASSWORD", "insecure"),
+    "tenant_name": "BGE000081_GOB",
+    "os_options": {
+        "tenant_id": "2ede4a78773e453db73f52500ef748e5",
+        "region_name": "NL",
+    },
+}
+
+
+@lru_cache(maxsize=None)
+def get_conn():
+    assert os.getenv("GOB_OBJECTSTORE_PASSWORD")
+    return Connection(**connection)
+
+
+def file_exists(target):
+    target = Path(target)
+    return target.is_file()
+
+
+def download_file(
+    file_path, target_path=None, target_root=settings.DATA_DIR, file_last_modified=None,
+):
+    path = file_path.split("/")
+
+    file_name = path[-1]
+    log.info(f"Create file {file_name} in {target_root}")
+    file_name = path[-1]
+
+    if target_path:
+        newfilename = "{}/{}".format(target_root, target_path)
+    else:
+        newfilename = "{}/{}".format(target_root, file_name)
+
+    if file_exists(newfilename):
+        log.debug("Skipped file exists: %s", newfilename)
+        return
+
+    with open(newfilename, "wb") as newfile:
+        data = get_conn().get_object(container, file_path)[1]
+        newfile.write(data)
+    if file_last_modified:
+        epoch_modified = file_last_modified.timestamp()
+        os.utime(newfilename, (epoch_modified, epoch_modified))
--- src/dso_api/datasets/bagh/batch.py
+++ src/dso_api/datasets/bagh/batch.py
@@ -1,10 +1,17 @@
+import logging
 import os
+
+from django.db.models import Count
+
 from dso_api import settings
-from dso_api.batch import batch
+from dso_api.batch import batch, csv, geo
+from dso_api.batch.objectstore import download_file
 from dso_api.datasets.models import Dataset
 
 GOB_CSV_ENCODING = "utf-8-sig"
 GOB_SHAPE_ENCODING = "utf-8"
+
+log = logging.getLogger(__name__)
 
 
 class ImportGemeenteTask(batch.BasicTask):
@@ -17,12 +24,12 @@ class ImportGemeenteTask(batch.BasicTask):
         (
             "03630000000000",
             1,
+            "0363",
             "1900-01-01 00:00:00.00000+00",
             "1900-01-01",
             "",
             "Amsterdam",
             "J",
-            "N",
         )
     ]
 
@@ -31,7 +38,7 @@ class ImportGemeenteTask(batch.BasicTask):
         self.model = models["gemeente"]
 
     def before(self):
-        pass
+        self.model.objects.all().delete()
 
     def after(self):
         pass
@@ -43,16 +50,110 @@ class ImportGemeenteTask(batch.BasicTask):
                 id=f"{r[0]}_{r[1]:03}",
                 identificatie=r[0],
                 volgnummer=r[1],
-                registratiedatum=r[2],
-                begin_geldigheid=r[3],
-                einde_geldigheid=r[4] or None,
-                naam=r[5],
-                verzorgingsgebied=r[6] == "J",
-                vervallen=r[7] == "J",
+                code=r[2],
+                registratiedatum=r[3],
+                begin_geldigheid=r[4],
+                eind_geldigheid=r[5] or None,
+                naam=r[6],
+                verzorgingsgebied=r[7] == "J",
             )
             for r in self.data
         ]
         self.model.objects.bulk_create(gemeentes, batch_size=100)
+
+
+class ImportWoonplaatsTask(batch.BasicTask):
+    name = "Import woonplaats"
+
+    def __init__(self, path, models):
+        self.path = path
+        self.models = models
+        self.model = models["woonplaats"]
+        self.filename = "BAG_woonplaats_ActueelEnHistorie.csv"
+        self.source_path = "bag/CSV_ActueelEnHistorie"
+        self.gemeentes = set()
+
+    def before(self):
+        self.model.objects.all().delete()
+        download_file(os.path.join(self.source_path, self.filename))
+        self.gemeentes = set(
+            self.models["gemeente"]
+            .objects.filter(eind_geldigheid__isnull=True)
+            .order_by("code")
+            .distinct("code")
+            .values_list("code", flat=True)
+        )
+
+    def after(self):
+        multiple_endranges = (
+            self.model.objects.values("identificatie")
+            .filter(eind_geldigheid__isnull=True)
+            .annotate(cnt=Count("identificatie"))
+            .filter(cnt__gt=1)
+        )
+
+        if len(multiple_endranges) > 0:
+            log.error(f"Multiple undefined eind_geldigheid for: {multiple_endranges}")
+
+        # TODO : Check overlapping time periods for each identificatie
+
+        self.gemeentes.clear()
+
+    def process(self):
+        source = os.path.join(self.path, self.filename)
+        woonplaatsen = csv.process_csv(
+            None, None, self.process_row, source=source, encoding=GOB_CSV_ENCODING
+        )
+
+        self.model.objects.bulk_create(woonplaatsen, batch_size=batch.BATCH_SIZE)
+
+    def process_row(self, r):
+        identificatie = r["identificatie"]
+        volgnummer = int(r["volgnummer"])
+        id = f"{identificatie}_{volgnummer:03}"
+        wkt_geometrie = r["geometrie"]
+        if wkt_geometrie:
+            geometrie = geo.get_multipoly(wkt_geometrie)
+            if not geometrie:
+                log.error(f"Woonplaats {id} has no valid geometry; skipping")
+                return None
+        else:
+            log.warning(f"OpenbareRuimte {id} has no geometry")
+            geometrie = None
+
+        gemeente_id = r["ligtIn:BRK.GME.identificatie"] or None
+        if gemeente_id and gemeente_id not in self.gemeentes:
+            log.error(
+                f"Woonplaats {id} has invalid gemeente_id {gemeente_id}; skipping"
+            )
+            return None
+        begin_geldigheid = csv.iso_datum_tijd(r["beginGeldigheid"])
+        eind_geldigheid = csv.iso_datum_tijd(r["eindGeldigheid"]) or None
+        if not csv.datum_geldig(begin_geldigheid, eind_geldigheid):
+            log.error(
+                f"Woonplaats {id} has invalid geldigheid {begin_geldigheid}-{eind_geldigheid}; skipping"  # noqa: E501
+            )
+            return None
+
+        values = {
+            "id": id,
+            "identificatie": r["identificatie"],
+            "volgnummer": r["volgnummer"],
+            "registratiedatum": csv.iso_datum_tijd(r["registratiedatum"]),
+            "begin_geldigheid": csv.iso_datum_tijd(r["beginGeldigheid"]),
+            "eind_geldigheid": csv.iso_datum_tijd(r["eindGeldigheid"]),
+            "aanduiding_in_onderzoek": csv.get_janee_boolean(
+                r["aanduidingInOnderzoek"]
+            ),
+            "geconstateerd": csv.get_janee_boolean(r["geconstateerd"]),
+            "naam": r["naam"],
+            "document_datum": csv.iso_datum(r["documentdatum"]),
+            "document_nummer": r["documentnummer"],
+            "status": r["status"],
+            "geometrie": geometrie,
+            "gemeente_identificatie": gemeente_id,
+        }
+        return self.model(**values)
 
 
 class ImportBagHJob(batch.BasicJob):
@@ -61,7 +162,7 @@ class ImportBagHJob(batch.BasicJob):
     def __init__(self, **kwargs):
         data_dir = settings.DATA_DIR
         if not os.path.exists(data_dir):
-            raise ValueError("GOB_DIR not found: {}".format(data_dir))
+            raise ValueError("DATA_DIR not found: {}".format(data_dir))
 
         self.data_dir = data_dir
         # For utf-8 files SHAPE_ENCODING needs to be set.
@@ -82,7 +183,7 @@ class ImportBagHJob(batch.BasicJob):
         return [
             # no-dependencies.
             ImportGemeenteTask(self.data_dir, self.models),
-            # ImportWoonplaatsTask(self.gob_bag_path),
+            ImportWoonplaatsTask(self.data_dir, self.models),
             # ImportStadsdeelTask(self.gob_gebieden_path),
             # ImportWijkTask(self.gob_gebieden_shp_path),
             #
