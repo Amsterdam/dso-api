116de5f55e90b32aa9ff32e45495bc7be64ab498
Bart Heupers <bart.heupers@gmail.com>
2020-03-26 13:30:56 +0100

    Process review feedback

--- src/dso_api/batch/csv.py
+++ src/dso_api/batch/csv.py
@@ -1,5 +1,5 @@
 import csv
-import datetime
+from datetime import datetime, date
 import logging
 import os
 from contextlib import contextmanager
@@ -9,49 +9,30 @@ log = logging.getLogger(__name__)
 GOB_CSV_ENCODING = "utf-8-sig"
 
 
-def iso_datum_tijd(s):
+def parse_date_time(s):
     if not s:
         return None
     if len(s) > 10:
-        pat = "%Y-%m-%dT%H:%M:%S"
+        return datetime.fromisoformat(s)
     else:
-        pat = "%Y-%m-%d"
-
-    return datetime.datetime.strptime(s, pat).date()
+        return date.fromisoformat(s)
 
 
-def get_janee_boolean(value):
-    return True if value == "J" else False if value == "N" else None
+def parse_yesno_boolean(value):
+    return True if value == "J" or value == "Y" else False if value == "N" else None
 
 
-def datum_geldig(start, eind):
-    return eind is None or start <= eind
-
-
-def _wrap_row(r, headers):
-    return dict(zip(headers, r))
+def is_valid_date_range(start, end):
+    return end is None or start <= end
 
 
 @contextmanager
 def _context_reader(
-    source,
-    quotechar=None,
-    quoting=csv.QUOTE_NONE,
-    with_header=True,
-    encoding=GOB_CSV_ENCODING,
+    source, quotechar=None, quoting=csv.QUOTE_NONE, encoding=GOB_CSV_ENCODING,
 ):
-
-    if not os.path.exists(source):
-        raise ValueError("File not found: {}".format(source))
-
     with open(source, encoding=encoding) as f:
-        rows = csv.reader(f, delimiter=";", quotechar=quotechar, quoting=quoting)
-
-        if with_header:
-            headers = next(rows)
-            yield (_wrap_row(r, headers) for r in rows)
-        else:
-            yield (r for r in rows)
+        rows = csv.DictReader(f, delimiter=";", quotechar=quotechar, quoting=quoting)
+        yield (r for r in rows)
 
 
 def logging_callback(source_path, original_callback):
@@ -66,9 +47,7 @@ def logging_callback(source_path, original_callback):
         try:
             return original_callback(r)
         except:  # noqa we reraise the exception.
-            log.error("Could not process row while parsing %s", source_path)
-            for k, v in r.items():
-                log.error("%s: '%s'", k, v)
+            log.error(f"Could not process row while parsing {source_path}: {r}")
             raise
 
     return result
@@ -78,7 +57,6 @@ def process_csv(
     path,
     file_name,
     process_row_callback,
-    with_header=True,
     quotechar='"',
     source=None,
     encoding="utf-8-sig",
@@ -91,11 +69,7 @@ def process_csv(
     cb = logging_callback(source, process_row_callback)
 
     with _context_reader(
-        source,
-        quotechar=quotechar,
-        quoting=csv.QUOTE_MINIMAL,
-        with_header=with_header,
-        encoding=encoding,
+        source, quotechar=quotechar, quoting=csv.QUOTE_MINIMAL, encoding=encoding,
     ) as rows:
         count = 0
         for row in rows:
