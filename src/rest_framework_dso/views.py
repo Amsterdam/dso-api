eaeae737a7e73484fc38ba53590ef86fddcfe644
Diederik van der Boor <vdboor@edoburu.nl>
2020-05-12 14:23:55 +0200

    Fix exception_handler() for real

--- src/rest_framework_dso/views.py
+++ src/rest_framework_dso/views.py
@@ -28,7 +28,7 @@ def exception_handler(exc, context):
         }
 
         response.content_type = "application/problem+json"
-    elif isinstance(response.get("detail"), ErrorDetail):
+    elif isinstance(response.data.get("detail"), ErrorDetail):
         # DRF parsed the exception as API
         detail = response.data["detail"]
         response.data = {
