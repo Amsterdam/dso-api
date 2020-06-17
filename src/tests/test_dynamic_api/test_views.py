efa9d301218bb47c11fcbaf7129284bcd7c911e8
Bart Heupers <bart.heupers@gmail.com>
2020-06-17 14:47:29 +0200

    Fix tests for authorization by clearing cache after tests

--- src/tests/test_dynamic_api/test_views.py
+++ src/tests/test_dynamic_api/test_views.py
@@ -5,6 +5,17 @@ from django.urls import reverse
 from rest_framework_dso.crs import CRS, RD_NEW
 from schematools.contrib.django import models
 from schematools.contrib.django.db import create_tables
+from dso_api.dynamic_api.permissions import (
+    fetch_scopes_for_dataset_table,
+    fetch_scopes_for_model,
+)
+
+
+@pytest.fixture(autouse=True)
+def clear_caches():
+    yield  # run tests first
+    fetch_scopes_for_dataset_table.cache_clear()
+    fetch_scopes_for_model.cache_clear()
 
 
 @pytest.mark.django_db
