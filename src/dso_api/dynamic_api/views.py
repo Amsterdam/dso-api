d5937912d26df4ddad270c40e14cd84a8d3a60bb
Bart Heupers <bart.heupers@gmail.com>
2020-06-17 09:07:44 +0200

    Add backwards relations

--- src/dso_api/dynamic_api/views.py
+++ src/dso_api/dynamic_api/views.py
@@ -178,7 +178,7 @@ def _get_ordering_fields(
 
 def viewset_factory(model: Type[DynamicModel]) -> Type[DynamicApiViewSet]:
     """Generate the viewset for a schema."""
-    serializer_class = serializers.serializer_factory(model)
+    serializer_class = serializers.serializer_factory(model, 0)
     filterset_class = filterset.filterset_factory(model)
     ordering_fields = _get_ordering_fields(serializer_class)
 
