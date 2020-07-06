0933b6b7d6cc7a924e2bbf2cf69f52aa0adb4803
Nick Garanko <ngaranko@gmail.com>
2020-07-06 16:37:24 +0200

    Fixing settings bleeding in tests.

--- src/tests/test_database_schemas_router.py
+++ src/tests/test_database_schemas_router.py
@@ -1,7 +1,6 @@
 import pytest
 from django.conf import settings
 from django.db import connections
-
 from dso_api.dbrouters import DatabaseSchemasRouter
 
 
@@ -12,7 +11,7 @@ class TestDatabaseSchemasRotuer:
         assert db_router.db_for_read(bagh_models["buurt"]) is None
 
     def test_db_for_read_returns_schema_name_if_set(self, bagh_schema, bagh_models):
-        default_settings = settings.DATABASE_SCHEMAS
+        default_settings = settings.DATABASE_SCHEMAS.copy()
         settings.DATABASE_SCHEMAS["bagh"] = "test"
         db_router = DatabaseSchemasRouter()
 
@@ -21,7 +20,7 @@ class TestDatabaseSchemasRotuer:
         settings.DATABASE_SCHEMAS = default_settings
 
     def test_db_for_read_creates_new_connection_if_set(self, bagh_schema, bagh_models):
-        default_settings = settings.DATABASE_SCHEMAS
+        default_settings = settings.DATABASE_SCHEMAS.copy()
         settings.DATABASE_SCHEMAS["bagh"] = "test"
         db_router = DatabaseSchemasRouter()
 
@@ -42,7 +41,7 @@ class TestDisableMigrationsRouter:
         assert db_router.allow_migrate(None, "bagh", "buurt") is None
 
     def test_allow_migrate_returns_false_if_set(self, bagh_schema, bagh_models):
-        default_settings = settings.DATABASE_DISABLE_MIGRATIONS
+        default_settings = settings.DATABASE_DISABLE_MIGRATIONS.copy()
         settings.DATABASE_DISABLE_MIGRATIONS = ["bagh"]
         db_router = DatabaseSchemasRouter()
 
