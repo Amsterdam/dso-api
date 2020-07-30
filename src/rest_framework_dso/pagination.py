a37a2c626000365f9fb97fdfc72be6f77dcc5fe7
Nick Garanko <ngaranko@gmail.com>
2020-07-30 14:40:58 +0200

    Page size Adjusted.

--- src/rest_framework_dso/pagination.py
+++ src/rest_framework_dso/pagination.py
@@ -14,6 +14,7 @@ class DSOPageNumberPagination(pagination.PageNumberPagination):
     https://tools.ietf.org/html/draft-kelly-json-hal-08
     """
 
+    page_size_query_param = "page_size"
     #: The field name for the results envelope
     results_field = None
 
