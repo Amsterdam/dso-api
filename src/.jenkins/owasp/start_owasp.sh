a3edcbbd309731b400b334844642eab25d91344c
Chris van Riel <c.van.riel@amsterdam.nl>
2020-09-24 10:50:41 +0200

    [439] added owasp package

--- src/.jenkins/owasp/start_owasp.sh
+++ src/.jenkins/owasp/start_owasp.sh
@@ -4,7 +4,7 @@ set -u # crash on missing env
 set -e # stop on any error
 
 echo "Waiting for OWASP"
-source .jenkins/test_owasp/docker-wait.sh
+source .jenkins/owasp/docker-wait.sh
 
 # echo "Running OWASP tests"
-DJANGO_DEBUG=false pytest .jenkins/test_owasp/test_owasp_api.py
+DJANGO_DEBUG=false pytest .jenkins/owasp/owasp.py
