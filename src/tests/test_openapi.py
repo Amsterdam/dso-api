Amsterdam/dso-api

commit 740ec93756583de31b88f8b854a3cadb0bc483ca

Include page/_pageSize/_format parameters in the OpenAPI spec

src/dso_api/dynamic_api/views/api.py
@@ -16,6 +16,8 @@ import inspect
 from django.db import models
 from django.http import Http404, JsonResponse
 from django.utils.translation import gettext as _
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import ExtraParameter, extend_schema
 from rest_framework import viewsets
 from typing import List, Type
 
@@ -137,6 +139,27 @@ class DynamicApiViewSet(
             return None
 
         return super().paginator
+
+    @extend_schema(
+        extra_parameters=[
+            ExtraParameter(
+                pagination_class.page_query_param,  # page=...
+                type=OpenApiTypes.INT,
+                location=ExtraParameter.QUERY,
+                description="Request a specific page",
+                required=False,
+            ).to_schema(),
+            ExtraParameter(
+                pagination_class.page_size_query_param,  # _pageSize
+                type=OpenApiTypes.INT,
+                location=ExtraParameter.QUERY,
+                description="Provide a custom page size, to deviate from the default",
+                required=False,
+            ).to_schema(),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super().finalize_response(request, response, *args, **kwargs)

src/rest_framework_dso/openapi.py
@@ -8,6 +8,7 @@ from drf_spectacular.types import OpenApiTypes, resolve_basic_type
 from drf_spectacular.utils import ExtraParameter
 from rest_framework import serializers
 from rest_framework.fields import ReadOnlyField
+from rest_framework.settings import api_settings
 from rest_framework_gis.fields import GeometryField
 
 from rest_framework_dso.fields import LinksField
@@ -286,4 +287,18 @@ class DSOAutoSchema(openapi.AutoSchema):
                 description="Content-Crs header for Geo queries",
                 required=False,
             ).to_schema(),
+            {
+                "in": ExtraParameter.QUERY,
+                "name": api_settings.URL_FORMAT_OVERRIDE,
+                "schema": {
+                    "type": "string",
+                    "enum": [
+                        renderer.format
+                        for renderer in self.view.renderer_classes
+                        if renderer.format != "api"  # Exclude browser view
+                    ],
+                },
+                "description": "Select the export format",
+                "required": False,
+            },
         ]

commit b11962400ba6f32cad8298fbc0f93a28cd434e4d

Fix OpenAPI page for list pagination parameters
Fixes bug introduced in 740ec93756583de31b88f8b854a3cadb0bc483ca.
The @extend_schema decorator completely overrides the superclass logic,
including any parameter details that were added by the filterset.

src/dso_api/dynamic_api/views/api.py
@@ -16,8 +16,6 @@ import inspect
 from django.db import models
 from django.http import Http404, JsonResponse
 from django.utils.translation import gettext as _
-from drf_spectacular.types import OpenApiTypes
-from drf_spectacular.utils import ExtraParameter, extend_schema
 from rest_framework import viewsets
 from typing import List, Type
 
@@ -139,27 +137,6 @@ class DynamicApiViewSet(
             return None
 
         return super().paginator
-
-    @extend_schema(
-        extra_parameters=[
-            ExtraParameter(
-                pagination_class.page_query_param,  # page=...
-                type=OpenApiTypes.INT,
-                location=ExtraParameter.QUERY,
-                description="Request a specific page",
-                required=False,
-            ).to_schema(),
-            ExtraParameter(
-                pagination_class.page_size_query_param,  # _pageSize
-                type=OpenApiTypes.INT,
-                location=ExtraParameter.QUERY,
-                description="Provide a custom page size, to deviate from the default",
-                required=False,
-            ).to_schema(),
-        ]
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
 
     def finalize_response(self, request, response, *args, **kwargs):
         response = super().finalize_response(request, response, *args, **kwargs)

src/rest_framework_dso/openapi.py
@@ -8,6 +8,7 @@ from drf_spectacular.types import OpenApiTypes, resolve_basic_type
 from drf_spectacular.utils import ExtraParameter
 from rest_framework import serializers
 from rest_framework.fields import ReadOnlyField
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.settings import api_settings
 from rest_framework_gis.fields import GeometryField
 
@@ -272,7 +273,7 @@ class DSOAutoSchema(openapi.AutoSchema):
 
     def get_extra_parameters(self, path, method):
         """Expose the DSO-specific HTTP headers in all API methods."""
-        return [
+        extra = [
             ExtraParameter(
                 "Accept-Crs",
                 type=OpenApiTypes.STR,
@@ -302,3 +303,30 @@ class DSOAutoSchema(openapi.AutoSchema):
                 "required": False,
             },
         ]
+
+        # Instead of exposing the extra parameters via drf-spectacular's @extend_schema,
+        # the pagination settings are exposed here in a general form. This also makes
+        # sure that the superclass logic is not ignored.
+        action = getattr(self.view, "action", method.lower())
+        pagination_class = getattr(self.view, "pagination_class", None)
+
+        if action == "list" and pagination_class is not None:
+            if issubclass(pagination_class, PageNumberPagination):
+                extra += [
+                    ExtraParameter(
+                        pagination_class.page_query_param,  # page=...
+                        type=OpenApiTypes.INT,
+                        location=ExtraParameter.QUERY,
+                        description="Request a specific page",
+                        required=False,
+                    ).to_schema(),
+                    ExtraParameter(
+                        pagination_class.page_size_query_param,  # _pageSize
+                        type=OpenApiTypes.INT,
+                        location=ExtraParameter.QUERY,
+                        description="Provide a custom page size, to deviate from the default",
+                        required=False,
+                    ).to_schema(),
+                ]
+
+        return extra

src/tests/test_openapi.py
@@ -41,10 +41,17 @@ def test_openapi_json(api_client, filled_router, caplog):
     # Prove that DSOOrderingFilter exposes parameters
     assert "_sort" in afval_parameters, ", ".join(afval_parameters.keys())
 
+    # Prove that general page parameters are exposed
+    assert "_pageSize" in afval_parameters, ", ".join(afval_parameters.keys())
+
     # Prove that the lookups of LOOKUPS_BY_TYPE are parsed
     # ([lt] for dates, [in] for keys)
     assert "datumCreatie[lt]" in afval_parameters, ", ".join(afval_parameters.keys())
     assert "clusterId[in]" in afval_parameters, ", ".join(afval_parameters.keys())
+
+    # Prove that the extra headers are included
+    assert "Accept-Crs" in afval_parameters, ", ".join(afval_parameters.keys())
+    assert afval_parameters["Accept-Crs"]["in"] == "header"
 
     assert not caplog.messages
 