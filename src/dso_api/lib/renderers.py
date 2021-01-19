dfa305f1bb9b8186389c3ddba45ee060e837eea5
Diederik van der Boor <vdboor@edoburu.nl>
2021-01-19 15:58:46 +0100

    Add renderer.unlimited_page_size attribute to avoid encoding format exceptions.

--- src/dso_api/lib/renderers.py
+++ src/dso_api/lib/renderers.py
@@ -1,7 +1,9 @@
 from rest_framework.renderers import BrowsableAPIRenderer
 
+from rest_framework_dso.renderers import RendererMixin
 
-class PatchedBrowsableAPIRenderer(BrowsableAPIRenderer):
+
+class PatchedBrowsableAPIRenderer(RendererMixin, BrowsableAPIRenderer):
     def get_context(self, data, accepted_media_type, renderer_context):
         context = super().get_context(data, accepted_media_type, renderer_context)
 
