2b51e8277a610c595ce0149e97c9517a3ed6a495
Diederik van der Boor <vdboor@edoburu.nl>
2021-03-16 15:55:08 +0100

    docs: fix docsstring errors for API documentation

--- src/rest_framework_dso/__init__.py
+++ src/rest_framework_dso/__init__.py
@@ -9,21 +9,27 @@ https://docs.geostandaarden.nl/api/API-Strategie-ext/
 
 Implemented:
 
-* HAL links {"_links": {"self": {"href": ..., "title": ...}}}
-* HAL ``?_expandScope=field1,field2`` -> gives ``_embedded`` field in response.
-* The ``?_expand=true`` option to expand all fields
-# The ``?_fields=...`` option to define which fields to return.
+* HAL links ``{"_links": {"self": {"href": ..., "title": ...}}}``
+* The ``?_expand=true`` option to sideload all related objects.
+* The ``?_expandScope=relation1,relation2`` to receive related objects in the same response.
+* The ``?_fields=...`` option to limit which fields to return,\
+  with the DSO 1.0 ``?fields=..`` as fallback.
+* The ``?_sort=...`` parameter, and the DSO 1.0 ``?sorteer=..`` as fallback
 * No envelope for single-object / detail views.
 
-Via other packages:
+Additionally:
 
-* Enforce fields in camelCase -> use djangorestframework-camel-case
+* We support ``?_pageSize=...`` to change the REST page size, with ``?page_size=..`` as fallback.
+* We support ``?_format=..`` to request other output formats\
+  (e.g. ``json``, ``geojson`` or ``csv``).
+* To write fields as "camelCase" either define the serializer as such,
+  or use djangorestframework-camel-case which rewrites the output.
 
-Not implemented:
+Not implemented (yet):
 
-* ?_fields=field1.subfield
-* ?_find=urgent (search queries, including ``*`` and ``?`` wildcards for single words)
-* GeoJSON support.
+* Subfields: ``?_fields=field1.subfield``
+* Wildcard search with ``?_find=...``
+* Queries with GeoJSON POST requests.
 
 Extra recommendations:
 
@@ -31,17 +37,24 @@ Extra recommendations:
 
 Mandatory settings:
 
-REST_FRAMEWORK = dict(
-    DEFAULT_PAGINATION_CLASS="rest_framework_dso.pagination.DSOPageNumberPagination",
-    DEFAULT_PARSER_CLASSES=[
-        "rest_framework_dso.parsers.HALJSONParser",
-        "rest_framework.parsers.JSONParser",
-        "rest_framework.parsers.FormParser",
-        "rest_framework.parsers.MultiPartParser",
-    ],
-    DEFAULT_RENDERER_CLASSES=[
-        "rest_framework_dso.renderers.HALJSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",  # <-- optional
-    ],
-)
+.. code-block:: python
+
+    REST_FRAMEWORK = dict(
+        PAGE_SIZE=20,
+        MAX_PAGINATE_BY=20,
+        DEFAULT_PAGINATION_CLASS="rest_framework_dso.pagination.DSOPageNumberPagination",
+        DEFAULT_SCHEMA_CLASS="rest_framework_dso.openapi.DSOAutoSchema",
+        DEFAULT_RENDERER_CLASSES=[
+            "rest_framework_dso.renderers.HALJSONRenderer",
+            "rest_framework_dso.renderers.CSVRenderer",
+            "rest_framework_dso.renderers.GeoJSONRenderer",
+            "rest_framework_dso.renderers.BrowsableAPIRenderer",  # Optional
+        ],
+        DEFAULT_FILTER_BACKENDS=[
+            "django_filters.rest_framework.backends.DjangoFilterBackend",
+        ],
+        EXCEPTION_HANDLER="rest_framework_dso.views.exception_handler",
+        COERCE_DECIMAL_TO_STRING=True,
+        URL_FORMAT_OVERRIDE="_format",  # use ?_format=.. instead of ?format=..
+    )
 """
