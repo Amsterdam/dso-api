007f3a3fd52114fbc8fa0e42d64d945d902ed02f
cvriel <chrisvanriel@gmail.com>
2021-03-17 15:40:25 +0100

    set DATABASE_URL for migrate specific with WRITE on master
    The API itself will look at the read slaves

--- src/.jenkins/docker-migrate.sh
+++ src/.jenkins/docker-migrate.sh
@@ -4,4 +4,5 @@ set -u   # crash on missing env variables
 set -e   # stop on any error
 
 echo "Migrating db"
+export DATABASE_URL=$DATABASE_URL_WRITE
 yes yes | INITIALIZE_DYNAMIC_VIEWSETS=0 python ./manage.py migrate --noinput
