7a0b7de77c18a1fd5b205ad48668a1ed1f439dd6
Diederik van der Boor <vdboor@edoburu.nl>
2021-03-18 14:34:48 +0100

    code cleanup: remove snake_to_camel_case() as schematools provides toCamelCase()
    This also avoids the risk that some toCamelCase() is missed due to
    conversion differences.

--- src/dso_api/dynamic_api/remote/serializers.py
+++ src/dso_api/dynamic_api/remote/serializers.py
@@ -7,9 +7,8 @@ from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 from schematools.types import DatasetFieldSchema, DatasetTableSchema
-from schematools.utils import to_snake_case
+from schematools.utils import to_snake_case, toCamelCase
 
-from dso_api.dynamic_api.utils import snake_to_camel_case
 from rest_framework_dso.fields import DSOGeometryField
 from rest_framework_dso.serializers import DSOListSerializer, DSOSerializer
 
@@ -102,7 +101,7 @@ def _build_declared_fields(
         # create converted field names on the serializer construction.
         # The space replacement is unlikely for a remote field, but kept for consistency.
         safe_field_name = field.name.replace(" ", "_")
-        camel_name = snake_to_camel_case(safe_field_name)
+        camel_name = toCamelCase(safe_field_name)
 
         kwargs = {"required": field.required, "allow_null": not field.required}
         if field.type == "string" and not field.required:
--- src/dso_api/dynamic_api/utils.py
+++ src/dso_api/dynamic_api/utils.py
@@ -1,24 +1,6 @@
 import re
 
-RE_CAMELIZE = re.compile(r"[a-z0-9]_[a-z0-9]")
-
 PK_SPLIT = re.compile("[_.]")
-
-
-def _underscore_to_camel(match):
-    chars = match.group()  # take complete match, it's only 3 chars
-    return chars[0] + chars[2].upper()
-
-
-def snake_to_camel_case(key: str) -> str:
-    """Convert snake_case to camelCase.
-
-    This logic is based on djangorestframework-camel-case.
-    instead of rewriting the response dynamically, this package changes the
-    field names at creation time. This is more efficient, and avoids patching
-    various other places that would otherwise response to snake_case input.
-    """
-    return re.sub(RE_CAMELIZE, _underscore_to_camel, key)
 
 
 def split_on_separator(value):
