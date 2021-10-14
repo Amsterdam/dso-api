3f03d0794ea7aceef3240886ad37d9e864c0a7d2
Diederik van der Boor <vdboor@edoburu.nl>
2021-10-14 12:11:03 +0200

    Fix support for dotted field.source in DynamicSerializer.get_fields()
    Added this to get_source_model_fields() instead, and updated all callers.
    As a consequence, it was easier to split the `get_fields()` method to
    `_apply_permission` and turn `get_request()` into a cached property.

--- src/dso_api/dynamic_api/utils.py
+++ src/dso_api/dynamic_api/utils.py
@@ -1,9 +1,11 @@
 import re
 from functools import lru_cache
 
+from django.db import models
 from django.db.models import Model
 from django.db.models.fields.related import RelatedField
 from django.db.models.fields.reverse_related import ForeignObjectRel
+from rest_framework import serializers
 from schematools.contrib.django.models import LooseRelationField
 from schematools.contrib.django.signals import dynamic_models_removed
 
@@ -44,3 +46,29 @@ def resolve_model_lookup(model: type[Model], lookup: str) -> tuple[type[Model], 
     # As the x_to_y flags are set on all field types, they also work for the LooseRelationField,
     # which is neither of these (it's technically a 'many_to_one' like ForeignKey).
     return model, (field.many_to_many or field.one_to_many)
+
+
+def get_source_model_fields(
+    serializer: serializers.ModelSerializer, field_name: str, field: serializers.Field
+) -> list[models.Field]:
+    """Find the model fields that the serializer field points to.
+    Typically this is only one field, but `field.source` could be set to a dotted path.
+    """
+    if field.source == "*":
+        # These fields are special: they receive the entire model instead of a attribute value.
+        raise ValueError("Unable to detect source for field.source == '*'")
+
+    orm_path = []
+
+    # Typically, `field.parent` and `field.source_attrs` are already set, making those arguments
+    # unnecessary. However, most use-cases of this function involve inspecting model data earlier
+    # in an override of serializer.get_fields(), which is before field.bind() is called.
+    model = serializer.Meta.model
+    source_attrs = (field.source or field_name).split(".")
+
+    for attr in source_attrs:
+        model_field = model._meta.get_field(attr)
+        model = model_field.related_model
+        orm_path.append(model_field)
+
+    return orm_path
