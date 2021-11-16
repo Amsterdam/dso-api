495a162452295452be851a2af9a72969e07e24b3
Yashar Robert <yashar@xs4all.nl>
2021-11-16 22:13:07 +0100

    AB#24657 Show token in swagger authorization screen
    A text area with the access token is show in the swagger authorization
    screen after a user has authorized. So it can be easily copied into a request header.

--- src/templates/dso_api/dynamic_api/swagger_ui.js
+++ src/templates/dso_api/dynamic_api/swagger_ui.js
@@ -1,4 +1,4 @@
-const swagger_settings  = {{settings|safe}}
+  const swagger_settings  = {{settings|safe}}
 
 const ui = SwaggerUIBundle({
   url: "{{schema_url|safe}}",
@@ -16,6 +16,19 @@ const ui = SwaggerUIBundle({
   },
   ...swagger_settings
 })
+
+// Show access token in swagger login screen
+window.dsoShowToken = (token) => {
+  let access_token = token.access_token
+  let el =  document.getElementsByClassName("auth-container")[0].getElementsByClassName("wrapper")[0]
+  el.innerHTML =`<div style="position:relative">
+                    <label>access token</label>
+                    <textarea rows=5 style="background-color:rgb(51,51,51); color:white; min-height:unset">${access_token}</textarea>
+                    <div class="copy-to-clipboard" style="bottom: 15px; right: 20px;" onclick="navigator.clipboard.writeText('${access_token}')">
+                      <button></button>
+                    </div>
+                  </div>`
+}
 
 /// Initiate Swagger UI with extra options.
 ui.initOAuth({
