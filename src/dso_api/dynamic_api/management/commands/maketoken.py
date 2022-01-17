d1ba9618b2328112b07c224ca69ef244b9d1edd1
jjmurre <jan.murre@gmail.com>
2022-01-17 14:24:29 +0100

    Add the `maketoken` script as Django mgm. command
    Courtesy lbam (from the readthedocs).

---
+++ src/dso_api/dynamic_api/management/commands/maketoken.py
@@ -0,0 +1,44 @@
+import json
+import sys
+import time
+from argparse import ArgumentParser
+from typing import Any
+
+from django.conf import settings
+from django.core.management import BaseCommand
+from jwcrypto.jwk import JWK
+from jwcrypto.jwt import JWT
+
+
+class Command(BaseCommand):
+    """Generate a JWT test token."""
+
+    help = """Generate a JWT test token to test endpoints that are protected by an auth scope.
+        See for usage: https://dso-api.readthedocs.io/en/latest/auth.html#testing
+    """
+
+    def add_arguments(self, parser: ArgumentParser) -> None:
+        """Hook to add arguments."""
+        parser.add_argument(
+            "args", metavar="scopes", nargs="*", help="Scopes that will be added to the JWT token"
+        )
+        parser.add_argument("--valid", default=1800, type=int, help="Validity period, in seconds")
+
+    def handle(self, *args: str, **options: Any) -> None:
+        """Main function of this command.
+
+        It creates a JWT test token with the provided scopes and validity.
+        """
+        key = JWK(**json.loads(settings.JWKS_TEST_KEY)["keys"][0])
+        scopes = list(set(args))
+        now = int(time.time())
+        claims = {
+            "iat": now,
+            "exp": now + options["valid"],
+            "scopes": scopes,
+            "sub": "test@tester.nl",
+        }
+        token = JWT(header={"alg": "ES256", "kid": key.key_id}, claims=claims)
+        token.make_signed_token(key)
+        sys.stdout.write(token.serialize())
+        sys.stdout.write("\n")
