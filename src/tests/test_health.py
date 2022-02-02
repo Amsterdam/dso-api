71d5a5ab653c17e66639ec37b36be4a1270f602e
jjmurre <jan.murre@gmail.com>
2022-02-02 10:09:41 +0100

    Add simple check to trigger 500 errors
    As a kind of reverse health check.

--- src/tests/test_health.py
+++ src/tests/test_health.py
@@ -5,3 +5,16 @@ import pytest
 def test_healthchecks(client):
     response = client.get("/status/health/")
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_runtime_error(client):
+    with pytest.raises(RuntimeError):
+        client.get("/500-test/")
+
+
+@pytest.mark.django_db
+def test_runtime_error_status_code(client):
+    client.raise_request_exception = False
+    response = client.get("/500-test/")
+    assert response.status_code == 500
