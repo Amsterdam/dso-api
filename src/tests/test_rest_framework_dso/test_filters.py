cb39daa6eebb1e338b4322616bd2e3cedbb9c806
LB <78799392+lbam@users.noreply.github.com>
2022-03-09 12:01:12 +0100

    Factor out SQL wildcard translation as a freestanding function (#481)
    Needed for the filter rewrite (AB#24838). Consistent with #459.

--- src/tests/test_rest_framework_dso/test_filters.py
+++ src/tests/test_rest_framework_dso/test_filters.py
@@ -5,25 +5,12 @@ from django.db.models import Value
 from django.http import QueryDict
 
 from rest_framework_dso.filters import DSOFilterBackend, DSOFilterSet
-from rest_framework_dso.filters.lookups import Wildcard
+from rest_framework_dso.filters.lookups import Wildcard, _sql_wildcards
 
 from .models import Category, Movie
 
 
 class TestWildcard:
-    def wildcard_escape(self, word):
-        # Quick shortcut to test the escaping of the lookup class
-        lookup = Wildcard("field", Value(word))
-        rhs, params = lookup.get_db_prep_lookup(word, connection=None)
-        return params[0]
-
-    def test_escapes_words(self):
-        assert self.wildcard_escape("foo*") == "foo%"
-        assert self.wildcard_escape("fo?o") == r"fo_o"
-        assert self.wildcard_escape("fo%o") == r"fo\%o"
-        assert self.wildcard_escape("fo%o_") == r"fo\%o\_"
-        assert self.wildcard_escape("f?_oob%ar*") == r"f_\_oob\%ar%"
-
     @pytest.mark.django_db
     def test_like_filter_sql(self, django_assert_num_queries):
         with django_assert_num_queries(1) as context:
@@ -33,6 +20,14 @@ class TestWildcard:
 
         sql = context.captured_queries[0]["sql"]
         assert r"""."name" LIKE 'foo%bar_'""" in sql
+
+
+def test_sql_wildcards():
+    assert _sql_wildcards("foo*") == "foo%"
+    assert _sql_wildcards("fo?o") == r"fo_o"
+    assert _sql_wildcards("fo%o") == r"fo\%o"
+    assert _sql_wildcards("fo%o_") == r"fo\%o\_"
+    assert _sql_wildcards("f?_oob%ar*") == r"f_\_oob\%ar%"
 
 
 class TestDSOFilterSet:
