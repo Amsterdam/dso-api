5797f996a2671659d9a140c0ef0f482d8ebcbf39
Diederik van der Boor <vdboor@edoburu.nl>
2022-07-13 14:31:53 +0200

    Request only the fields that the client accesses
    This avoids requesting fields for which access is denied
    (so database rules can be added). It also works when the ?_fields=..
    parameter is given, as it checks which fields the serializer will read.
    
    The prefetch-related queries are also updated to fetch a limited set of
    fields. The resolve_model_lookup() now returns the field instead of the
    model, since both field, related_model and is_many fields are needed.

--- src/dso_api/dynamic_api/utils.py
+++ src/dso_api/dynamic_api/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import re
 from functools import lru_cache
 
@@ -8,6 +10,8 @@ from django.db.models.fields.reverse_related import ForeignObjectRel
 from rest_framework import serializers
 from schematools.contrib.django.models import LooseRelationField
 from schematools.contrib.django.signals import dynamic_models_removed
+
+from rest_framework_dso.fields import GeoJSONIdentifierField
 
 PK_SPLIT = re.compile("[_.]")
 
@@ -23,16 +27,16 @@ dynamic_models_removed.connect(lambda **kwargs: resolve_model_lookup.cache_clear
 
 
 @lru_cache
-def resolve_model_lookup(model: type[Model], lookup: str) -> tuple[type[Model], bool]:
-    """Find which model a lookup points to.
-
-    :returns: The model that the relation points to,
-        and whether reading the relation could return multiple objects.
+def resolve_model_lookup(
+    model: type[Model], lookup: str
+) -> list[RelatedField | ForeignObjectRel | LooseRelationField]:
+    """Find which fields a lookup points to.
+    :returns: The model fields that the relation traverses.
     """
     if not lookup:
         raise ValueError("Empty lookup can't be resolved")
 
-    field = None
+    fields = []
     for field_name in lookup.split("__"):
         field = model._meta.get_field(field_name)
 
@@ -43,9 +47,73 @@ def resolve_model_lookup(model: type[Model], lookup: str) -> tuple[type[Model], 
         else:
             raise ValueError(f"Field '{field}' is not a relation in lookup '{lookup}'")
 
-    # As the x_to_y flags are set on all field types, they also work for the LooseRelationField,
-    # which is neither of these (it's technically a 'many_to_one' like ForeignKey).
-    return model, (field.many_to_many or field.one_to_many)
+        fields.append(field)
+
+    return fields
+
+
+def get_serializer_source_fields(  # noqa: C901
+    serializer: serializers.BaseSerializer, prefix=""
+) -> list[str]:
+    """Find which ORM fields a serializer instance would request.
+
+    It checks "serializer.fields", and analyzes each "field.source" to find
+    the model attributes that would be read.
+    This allows to prepare an ``only()`` call on the queryset.
+    """
+    # Unwrap the list serializer construct for the one-to-many relationships.
+    if isinstance(serializer, serializers.ListSerializer):
+        serializer = serializer.child
+
+    lookups = []
+
+    for field in serializer.fields.values():  # type: serializers.Field
+        if field.source == "*":
+            # Field takes the full object, only some cases are supported:
+            if isinstance(field, serializers.BaseSerializer):
+                # When a serializer receives the same data as the parent instance, it can be
+                # seen as being a part of the parent. The _links field is implemented this way.
+                lookups.extend(get_serializer_source_fields(field, prefix=prefix))
+            elif isinstance(field, serializers.HyperlinkedRelatedField):
+                # Links to an object e.g. TemporalHyperlinkedRelatedField
+                # When the lookup_field matches the identifier, there is no need
+                # to add the field because the parent name is already includes it.
+                if field.lookup_field != "pk":
+                    lookups.append(f"{prefix}{field.lookup_field}")
+            elif field.field_name == "schema" or isinstance(field, GeoJSONIdentifierField):
+                # Fields that do have "source=*", but don't read any additional fields.
+                # e.g. SerializerMethodField for schema, and GeoJSON ID field.
+                continue
+            elif isinstance(field, serializers.CharField):
+                # A CharField(source="*") that either receives a str from its parent
+                # (e.g. _loose_link_serializer_factory() receives a str as data),
+                # or it's reading DynamicModel.__str__().
+                if isinstance(serializer, serializers.ModelSerializer) and (
+                    display_field := serializer.Meta.model.table_schema().display_field
+                ):
+                    lookups.append(f"{prefix}{display_field}")
+                continue
+            else:
+                raise NotImplementedError(
+                    f"Can't determine .only() for {prefix}{field.field_name}"
+                    f" ({field.__class__.__name__})"
+                )
+        else:
+            # Check the field isn't a reverse field, this should not be mentioned at all
+            # because such field doesn't access a local field (besides the primary key).
+            model_fields = get_source_model_fields(serializer, field.field_name, field)
+            if isinstance(model_fields[0], models.ForeignObjectRel):
+                continue
+
+            # Regular field: add the source value to the list.
+            lookup = f"{prefix}{'__'.join(field.source_attrs)}"
+            lookups.append(lookup)
+
+            if isinstance(field, (serializers.ModelSerializer, serializers.ListSerializer)):
+                lookups.extend(get_serializer_source_fields(field, prefix=f"{lookup}__"))
+
+    # Deduplicate the final result, as embedded fields could overlap with _links.
+    return sorted(set(lookups)) if not prefix else lookups
 
 
 def get_source_model_fields(
--- src/tests/test_dynamic_api/test_utils.py
+++ src/tests/test_dynamic_api/test_utils.py
@@ -20,9 +20,13 @@ def test_split(instr, result):
 
 def _assert_lookup(start_model, lookup, expect):
     # Making repeated assertions easier.
-    result = resolve_model_lookup(start_model, lookup)
-    assert result == expect, f"Failed resolving {start_model.__name__}.{lookup.replace('__', '.')}"
-    assert result[0] is expect[0]  # Confirm object references too
+    expect_model, expect_is_many = expect
+    model_field = resolve_model_lookup(start_model, lookup)[-1]
+
+    msg = f"Failed resolving {start_model.__name__}.{lookup.replace('__', '.')}"
+    assert model_field.related_model == expect_model, msg
+    assert (model_field.one_to_many or model_field.many_to_many) == expect_is_many, msg
+    assert model_field.related_model is expect_model  # Confirm object references too
 
 
 def test_resolve_model_lookup():
