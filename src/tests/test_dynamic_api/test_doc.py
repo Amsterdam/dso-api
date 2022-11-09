a3f852c839d72d777a55df9b8f94e60dd9477025
LB <78799392+lbam@users.noreply.github.com>
2022-11-09 12:08:12 +0100

    Dataset docs: generate URLs using the router

--- src/tests/test_dynamic_api/test_doc.py
+++ src/tests/test_dynamic_api/test_doc.py
@@ -13,3 +13,18 @@ def test_overview(api_client, fietspaaltjes_dataset):
 
     content = response.rendered_content
     assert """<a href="datasets/fietspaaltjes.html#fietspaaltjes">Fietspaaltjes</a>""" in content
+
+
+@pytest.mark.django_db
+def test_dataset(api_client, filled_router, gebieden_dataset):
+    """Tests documentation for a single dataset."""
+    gebieden_doc = reverse("dynamic_api:doc-gebieden")  # Gebieden has relationships between its tables.
+    assert gebieden_doc
+
+    response = api_client.get(gebieden_doc)
+    assert response.status_code == 200
+    content = response.rendered_content
+
+    # Check for self-link to wijken.
+    assert """<a id="wijken">""" in content
+    assert """<a href="/v1/docs/datasets/gebieden.html#wijken">gebieden:wijken:ligtInStadsdeel</a>""" in content
