485e51c8eadca2f3a46a0c386fcc4e2c00a66b06
DonQueso89 <k.van.ekeren@amsterdam.nl>
2022-11-16 18:53:29 +0100

    fix script

--- src/get_token.sh
+++ src/get_token.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-echo $(curl -s "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fossrdbms-aad.database.windows.ne" -H Metadata:true | python -c 'import json, sys; print(json.loads(sys.stdin.read())["access_token"])')
+echo $(curl -s "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fossrdbms-aad.database.windows.net" -H 'Metadata: true' | python -c 'import json, sys; print(json.loads(sys.stdin.read())["access_token"])')
