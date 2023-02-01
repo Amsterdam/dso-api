23ec8b2dcafcb1cd807adabf39172c9693d6ff47
LB <78799392+lbam@users.noreply.github.com>
2023-02-01 11:26:29 +0100

    Revert new Docker image (#688)
    Gives trouble with Content-Types not being set. No idea why.

--- src/dso_api/wsgi.py
+++ src/dso_api/wsgi.py
@@ -9,11 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from django.conf import settings
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dso_api.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
