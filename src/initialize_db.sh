8838a6f9730ffc0fedfe7f42e582b6dbcf3657be
Yashar Robert <y.robert@amsterdam.nl>
2024-08-01 14:53:32 +0200

    Add mock data option to compose file
    Add a configuration to the compose file that fills the database with mock data.

--- src/initialize_db.sh
+++ src/initialize_db.sh
@@ -5,4 +5,11 @@ then
     ./manage.py migrate;
     ./manage.py import_schemas --create-tables;
     schema permissions apply --auto --revoke --create-roles --execute  -a "datasets_dataset:SELECT;scope_openbaar" -a "datasets_datasettable:SELECT;scope_openbaar" -a "datasets_datasetfield:SELECT;scope_openbaar"  -a "datasets_datasetprofile:SELECT;scope_openbaar";
+
+    # Fill tables with mock data if MOCK_DATA is set. Continue on errors.
+    if "$MOCK_DATA" = "true";
+    then
+        django create_mock_data --size 20 --exclude None || true;
+        django relate_mock_data --exclude None || true;
+    fi
 fi
