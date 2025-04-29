0164b570c192325f100c2b0d0127a7d6db078057
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-04-29 15:29:01 +0200

    bump schematools to v7.6.2 (#1001)
    * bump schematools to v7.6.2
    
    * increase sleep to 15
    
    * execute imports

--- src/initialize_db.sh
+++ src/initialize_db.sh
@@ -3,7 +3,7 @@
 if ! django migrate --check && "$INITIALIZE_DB" = "true";
 then
     ./manage.py migrate;
-    ./manage.py import_schemas --create-tables;
+    ./manage.py import_schemas --create-tables --execute;
     schema permissions apply --auto --revoke --create-roles --execute  -a "datasets_dataset:SELECT;scope_openbaar" -a "datasets_datasettable:SELECT;scope_openbaar" -a "datasets_datasetfield:SELECT;scope_openbaar"  -a "datasets_datasetprofile:SELECT;scope_openbaar";
 
     # Fill tables with mock data if MOCK_DATA is set. Continue on errors.
