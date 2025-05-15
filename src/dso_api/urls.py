9f40cbb5ed103e7d2edb9d247182dc1eaea2f547
Diederik van der Boor <vdboor@edoburu.nl>
2025-05-15 14:51:08 +0200

    docs: add redirects for folder names

--- src/dso_api/urls.py
+++ src/dso_api/urls.py
@@ -12,13 +12,17 @@ def _raise_runtime_error(request):
     raise RuntimeError("Testing 500 error.")
 
 
+docs_index_redirect = RedirectView.as_view(pattern_name="dynamic_api:docs-index", permanent=True)
+
 urlpatterns = [
     path("status/health/", include(django_healthchecks.urls)),
     path("500-test/", _raise_runtime_error),
     path("v1", include(dso_api.dynamic_api.urls), name="root"),
     path("v1/", RedirectView.as_view(url="/v1", permanent=True), name="root-tailing-slash"),
     path("", RedirectView.as_view(url="/v1"), name="root-redirect"),
-    path("v1/docs/", RedirectView.as_view(url="/v1/docs/index.html", permanent=True)),
+    path("v1/docs/", docs_index_redirect),
+    path("v1/docs/datasets/", docs_index_redirect),
+    path("v1/docs/datasets/index.html", docs_index_redirect),
     re_path(
         r"^.*/{2,}.*$",
         views.multiple_slashes,
