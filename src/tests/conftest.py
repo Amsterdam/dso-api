564ec1badba8f02ee52c3fa0ea5149511840ef97
Jasper Jager <j.jager@amsterdam.nl>
2025-12-08 15:29:42 +0100

    Add testcase with camelcased display_field

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1313,6 +1313,7 @@ def stadsdelen_data(stadsdelen_model) -> DynamicModel:
         begin_geldigheid=DATE_2021_FEB,
         naam="Centrum",
         code="A",
+        opgemaakte_naam="Centrum",
     )
 
 
