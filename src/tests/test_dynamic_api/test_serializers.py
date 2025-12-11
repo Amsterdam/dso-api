a3f8c674c4d7b78a22af3b66fc870b1543e76aef
Jasper Jager <j.jager@amsterdam.nl>
2025-12-11 15:46:59 +0100

    Revert "add tests, expand(!) serializer validation with case for nested relations"
    This reverts commit 1cd8fcaada0dd67c9488cd26449a1cd2165f64ea.

--- src/tests/test_dynamic_api/test_serializers.py
+++ src/tests/test_dynamic_api/test_serializers.py
@@ -376,7 +376,6 @@ class TestDynamicSerializer:
         wijken_model.objects.create(
             id="03630000000001.1",
             identificatie="03630000000001",
-            naam="Burgwallen-Oude Zijde",
             volgnummer=1,
             ligt_in_stadsdeel=stadsdeel,
         )
@@ -398,7 +397,7 @@ class TestDynamicSerializer:
                 "wijken": [
                     {
                         "href": "http://testserver/v1/gebieden/wijken/03630000000001?volgnummer=1",
-                        "title": "Burgwallen-Oude Zijde",
+                        "title": "03630000000001.1",
                         "volgnummer": 1,
                         "identificatie": "03630000000001",
                     }
