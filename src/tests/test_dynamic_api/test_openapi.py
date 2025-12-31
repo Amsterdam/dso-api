d7beaf7129f4b6da0384b48cf860902fb443dc28
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-12-31 15:28:55 +0100

    remove parent_lookup_ prefix from url params

--- src/tests/test_dynamic_api/test_openapi.py
+++ src/tests/test_dynamic_api/test_openapi.py
@@ -266,6 +266,36 @@ def test_openapi_json(api_client, afval_dataset, fietspaaltjes_dataset, filled_r
 
 
 @pytest.mark.django_db
+def test_openapi_json_subresources(api_client, gebieden_subresources_dataset, filled_router):
+    """Assert that nested paths are in the openapi json."""
+    url = reverse(
+        "dynamic_api:openapi-version",
+        kwargs={"dataset_name": "gebieden_subresources"},
+    )
+    response = api_client.get(url)
+    assert response.status_code == 200
+    schema = response.data
+
+    openapi_spec_validator.validate_spec(schema)
+    # Prove that only afvalwegingen are part of this OpenAPI page:
+    paths = sorted(schema["paths"].keys())
+    assert paths == [
+        "/buurten",
+        "/buurten/{id}",
+        "/stadsdelen",
+        "/stadsdelen/{id}",
+        "/stadsdelen/{id}/wijken",
+        "/stadsdelen/{id}/wijken/{id}",
+        "/stadsdelen/{id}/wijken/{id}/buurten",
+        "/stadsdelen/{id}/wijken/{id}/buurten/{id}",
+        "/wijken",
+        "/wijken/{id}",
+        "/wijken/{id}/buurten",
+        "/wijken/{id}/buurten/{id}",
+    ]
+
+
+@pytest.mark.django_db
 def test_openapi_json_v1(api_client, afval_dataset, fietspaaltjes_dataset, filled_router, caplog):
     """Prove that the OpenAPI page can be rendered."""
     caplog.set_level(logging.WARNING)
