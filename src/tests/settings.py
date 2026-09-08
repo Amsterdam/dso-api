6b16d2c478ddaaf52bdb15e0d2fe2b08a7abb1a7
Pieter Stokkink <p.stokkink@amsterdam.nl>
2026-09-08 15:21:25 +0200

    Revert "add TRUSTED_JWKS settings"
    This reverts commit c873943e07fc33c55037122d84d27517b43c4062.

--- src/tests/settings.py
+++ src/tests/settings.py
@@ -36,7 +36,7 @@ SESSION_COOKIE_SECURE = False
 jwks_key = Path(__file__).parent.parent.joinpath("jwks_test.json").read_text()
 
 DATAPUNT_AUTHZ = {
-    "TRUSTED_JWKS": [{"jwks": jwks_key, "claims": {"iss": "iss"}}],
+    "JWKS": jwks_key,
     "ALWAYS_OK": False,
     "MIN_INTERVAL_KEYSET_UPDATE": 30 * 60,  # 30 minutes
 }
